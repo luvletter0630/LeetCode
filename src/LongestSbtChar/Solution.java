1fdea0f2b26a9eb31e7557d67b9c5694236e78e4
liwj <761644814@qq.com>
2019-03-18 17:32:24 +0800

    LeetCode practice

--- src/LongestSbtChar/Solution.java
+++ src/LongestSbtChar/Solution.java
@@ -1,5 +1,22 @@
 package LongestSbtChar;
 
-public class Solution {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Solution {
+    public static int lengthOfLongestSubstring(String s) {
+        char [] arry = s.toCharArray();
+        Map map = new HashMap();
+        int cnt = 0;
+        for (int i = 0; i < arry.length; i ++){
+            if (map.containsKey(arry[i])){
+                if (cnt < map.size()){
+                    cnt = map.size();
+                }
+                map.clear();
+            }
+            map.put(arry[i],i);
+        }
+        return cnt;
+    }
 }
